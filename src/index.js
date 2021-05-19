b36f7fa7e7e9fdb0987fed2826e159598a4b858f
Anton Bazhal <AntonBazhal@users.noreply.github.com>
2021-05-19 18:55:26 -0400

    Implementation (#1)
    * Implementation
    
    * Docs
    
    * Minor tweaks
    
    * PR feedback

--- src/index.js
+++ src/index.js
@@ -1,9 +1,159 @@
 'use strict';
 
+const Ajv = require('ajv');
+const get = require('lodash.get');
+const set = require('lodash.set');
+
+const EXCLUDE_PATHS = new Set([
+	'serverless', 'serviceObject', 'pluginsData', 'package',
+	'configValidationMode', 'disabledDeprecations', 'serviceFilename', 'app',
+	'tenant', 'org', 'initialServerlessConfig'
+]);
+
+const SCHEMA = {
+	type: 'object',
+	properties: {
+		iterator: {
+			oneOf: [
+				{
+					type: ['array']
+				},
+				{
+					type: 'object',
+					oneOf: [
+						{
+							properties: {
+								$env: {
+									type: 'string'
+								}
+							},
+							required: ['$env'],
+							additionalProperties: false
+						},
+						{
+							not: {
+								required: ['$env']
+							},
+							additionalProperties: true,
+						}
+					]
+				}
+			]
+		},
+		template: {
+			type: ['array', 'object']
+		}
+	},
+	required: ['iterator', 'template'],
+	additionalProperties: false
+};
+
 class ForEachPlugin {
-	constructor(serverless, options) {
+	constructor(serverless) {
 		this.serverless = serverless;
-		this.options = options;
+
+		this.ajv = new Ajv({ allowUnionTypes: true });
+		this.validator = this.ajv.compile(SCHEMA);
+
+		this.hooks = {
+			'after:package:initialize': this.replace.bind(this),
+		};
+
+		if (this.serverless.configSchemaHandler && this.serverless.configSchemaHandler.defineFunctionEvent) {
+			this.serverless.configSchemaHandler.defineFunctionEvent('aws', '$forEach', SCHEMA);
+		}
+	}
+
+	interpolate(template, key, value) {
+		const stringified = JSON.stringify(template);
+
+		const interpolated = stringified
+			.replace(/\$forEach.key/g, key)
+			.replace(/\$forEach.value/g, value);
+
+		try {
+			return JSON.parse(interpolated);
+		} catch (error) {
+			throw new Error(`Interpolated template is not a valid JSON: ${interpolated}`);
+		}
+	}
+
+	findAndReplace(obj, path) {
+		if (Array.isArray(obj)) {
+			obj.forEach((val, idx) => this.findAndReplace(val, `${path}[${idx}]`));
+		} else if (typeof obj === 'object' && obj !== null) {
+			for (const key in obj) {
+				if (key === '$forEach') {
+					this.log(`Found a match in ${path}`);
+
+					this.validate(obj[key], `${path}/${key}`);
+
+					const { iterator: rawIterator, template } = obj[key];
+
+					const iterator = {};
+					if (rawIterator.$env) {
+						Object.entries(process.env).forEach(([name, value]) => {
+							if (name.match(rawIterator.$env)) {
+								iterator[name] = value;
+							}
+						});
+					} else if (Array.isArray(rawIterator)) {
+						rawIterator.forEach((value, idx) => iterator[idx] = value);
+					} else {
+						Object.assign(iterator, rawIterator);
+					}
+
+					const interpolated = Object.entries(iterator).reduce((acc, [key, value]) => {
+						const entry = this.interpolate(template, key, value);
+
+						return Array.isArray(template)
+							? acc.concat(entry)
+							: { ...acc, ...entry };
+					}, Array.isArray(template) ? [] : {});
+
+					const pathIsArray = path.match(/^(.+)\[(\d+)\]$/);
+
+					if (pathIsArray && Array.isArray(interpolated)) {
+						const basePath = pathIsArray[1];
+						const index = Number(pathIsArray[2]);
+
+						get(this.serverless.service, basePath).splice(index, 1, ...interpolated);
+					} else {
+						const { $forEach, ...result } = obj; // eslint-disable-line no-unused-vars
+
+						if (Array.isArray(interpolated) && Object.keys(result).length > 0) {
+							throw new Error('Can\'t merge array into object');
+						}
+
+						set(this.serverless.service, path, { ...result, ...interpolated });
+					}
+				} else {
+					this.findAndReplace(obj[key], `${path}.${key}`);
+				}
+			}
+		}
+	}
+
+	validate(config, path) {
+		if (!this.validator(config)) {
+			throw new Error(this.ajv.errorsText(this.validator.errors, { dataVar: path }));
+		}
+	}
+
+	replace() {
+		this.log('Scanning configuration');
+
+		Object.entries(this.serverless.service).forEach(([path, value]) => {
+			if (!EXCLUDE_PATHS.has(path)) {
+				this.findAndReplace(value, path);
+			}
+		});
+
+		this.log('Scan complete');
+	}
+
+	log(message) {
+		this.serverless.cli.log(`[serverless-plugin-for-each] ${message}`);
 	}
 }
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,414 @@
 'use strict';
 
 const { expect } = require('chai');
+const sinon = require('sinon');
 
 const ForEachPlugin = require('../src/index');
 
+function createTestInstance(service, other = {}) {
+	const serverless = {
+		service: {
+			functions: [],
+			provider: {},
+			...service
+		},
+		cli: {
+			log: sinon.stub()
+		},
+		...other
+	};
+
+	return {
+		plugin: new ForEachPlugin(serverless),
+		serverless
+	};
+}
+
 describe('ForEachPlugin', function() {
-	it('should create an instance', function() {
-		expect(() => new ForEachPlugin())
-			.to.not.throw();
+	afterEach(function() {
+		sinon.restore();
+	});
+
+	describe('should throw validation error when', function() {
+		[
+			{
+				scenario: 'config is not an object',
+				config: [],
+				message: 'custom/$forEach must be object'
+			},
+			{
+				scenario: 'required keys are missing',
+				config: {},
+				message: 'custom/$forEach must have required property \'iterator\''
+			},
+			{
+				scenario: 'additional keys are present',
+				config: {
+					iterator: [],
+					template: [''],
+					junk: ''
+				},
+				message: 'custom/$forEach must NOT have additional properties'
+			},
+			{
+				scenario: 'iterator type is invalid',
+				config: {
+					iterator: '',
+					template: ['']
+				},
+				message: 'custom/$forEach/iterator must be array, custom/$forEach/iterator must be object, custom/$forEach/iterator must match exactly one schema in oneOf'
+			},
+			{
+				scenario: 'iterator has $env property together with some other property',
+				config: {
+					iterator: {
+						$env: 'foo',
+						junk: ''
+					},
+					template: ['']
+				},
+				message: 'custom/$forEach/iterator must NOT have additional properties'
+			},
+			{
+				scenario: 'iterator has $env property that is not a string',
+				config: {
+					iterator: {
+						$env: 12345
+					},
+					template: ['']
+				},
+				message: 'custom/$forEach/iterator must be array, custom/$forEach/iterator/$env must be string'
+			},
+			{
+				scenario: 'template type is invalid',
+				config: {
+					iterator: [''],
+					template: ''
+				},
+				message: 'custom/$forEach/template must be array,object'
+			}
+		].forEach(({ scenario, config, message }) => {
+			it(scenario, function() {
+				const { plugin } = createTestInstance({
+					custom: {
+						$forEach: config
+					}
+				});
+
+				expect(
+					() => plugin.replace()
+				).to.throw(message);
+			});
+		});
+	});
+
+	it('should define function event schema when defineFunctionEvent method is available', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				foo: 'foo',
+				$forEach: {
+					iterator: {
+						bar: 'bar',
+						baz: 'baz'
+					},
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			}
+		}, {
+			configSchemaHandler: {
+				defineFunctionEvent: sinon.stub()
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		sinon.assert.callCount(serverless.configSchemaHandler.defineFunctionEvent, 1);
+		sinon.assert.calledWithExactly(serverless.configSchemaHandler.defineFunctionEvent,
+			'aws',
+			'$forEach',
+			sinon.match.object
+		);
+	});
+
+	it('should handle defineFunctionEvent being not defined', function() {
+		const { plugin } = createTestInstance({
+			custom: {
+				foo: 'foo',
+				$forEach: {
+					iterator: {
+						bar: 'bar',
+						baz: 'baz'
+					},
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			}
+		}, {
+			configSchemaHandler: {}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+	});
+
+	it('should support object key replacement', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				foo: 'foo',
+				$forEach: {
+					iterator: {
+						bar: 'bar',
+						baz: 'baz'
+					},
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom).to.deep.equal({
+			foo: 'foo',
+			bar: 'bar',
+			baz: 'baz'
+		});
+	});
+
+	it('should support array item replacement', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				foo: [
+					{ before: 'before' },
+					{
+						$forEach: {
+							iterator: {
+								bar: 'bar',
+								baz: 'baz'
+							},
+							template: [
+								{ '$forEach.key': '$forEach.value' }
+							]
+						}
+					},
+					{ after: 'after' }
+				]
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom.foo).to.deep.equal([
+			{ before: 'before' },
+			{ bar: 'bar' },
+			{ baz: 'baz' },
+			{ after: 'after' }
+		]);
+	});
+
+	it('should flatten one level when replacing array item and template is an array', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				foo: [
+					{
+						$forEach: {
+							iterator: {
+								bar: 'bar',
+								baz: 'baz'
+							},
+							template: [
+								{ '$forEach.key': '$forEach.value' }
+							]
+						}
+					}
+				]
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom.foo).to.deep.equal([
+			{ bar: 'bar' },
+			{ baz: 'baz' }
+		]);
+	});
+
+	it('should support environment variable regex iterator', function() {
+		sinon.stub(process, 'env').value({
+			...process.env,
+			FOR_EACH_TEST1: 'foo',
+			FOR_EACH_TEST2: 'bar',
+			not_FOR_EACH_TEST2: 'baz'
+		});
+
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				$forEach: {
+					iterator: {
+						$env: '^FOR_EACH_TEST'
+					},
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom).to.deep.equal({
+			FOR_EACH_TEST1: 'foo',
+			FOR_EACH_TEST2: 'bar'
+		});
+	});
+
+	it('should support array iterator', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				$forEach: {
+					iterator: ['foo', 'bar'],
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom).to.deep.equal({
+			0: 'foo',
+			1: 'bar'
+		});
+	});
+
+	it('should support object iterator', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				$forEach: {
+					iterator: {
+						fookey: 'foo',
+						barkey: 'bar'
+					},
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom).to.deep.equal({
+			fookey: 'foo',
+			barkey: 'bar'
+		});
+	});
+
+	it('should exclude paths that should not be traversed', function() {
+		const pathThatShouldNotBeTraversed = 'serverless';
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				$forEach: {
+					iterator: {
+						fookey: 'foo',
+						barkey: 'bar'
+					},
+					template: {
+						'$forEach.key': '$forEach.value'
+					}
+				}
+			},
+			[pathThatShouldNotBeTraversed]: {
+				foo: 'foo'
+			}
+		});
+
+		sinon.spy(plugin, 'findAndReplace');
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service).to.have.property(pathThatShouldNotBeTraversed);
+
+		sinon.assert.called(plugin.findAndReplace);
+		sinon.assert.neverCalledWith(plugin.findAndReplace,
+			sinon.match.any,
+			pathThatShouldNotBeTraversed
+		);
+	});
+
+	it('should replace all interpolation variables', function() {
+		const { plugin, serverless } = createTestInstance({
+			custom: {
+				$forEach: {
+					iterator: ['foo'],
+					template: {
+						result: '$forEach.key:$forEach.key:$forEach.value:$forEach.value'
+					}
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.not.throw();
+
+		expect(serverless.service.custom).to.deep.equal({
+			result: '0:0:foo:foo'
+		});
+	});
+
+	it('throw an error when interpolated template is not a valid JSON', function() {
+		const { plugin } = createTestInstance({
+			custom: {
+				$forEach: {
+					iterator: ['\\'],
+					template: {
+						'$forEach.value': '$forEach.value'
+					}
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.throw('Interpolated template is not a valid JSON');
+	});
+
+	it('throw an error when interpolated template is an array, but $forEach was a part of the object', function() {
+		const { plugin } = createTestInstance({
+			custom: {
+				foo: 'foo',
+				$forEach: {
+					iterator: ['bar'],
+					template: ['$forEach.value']
+				}
+			}
+		});
+
+		expect(
+			() => plugin.replace()
+		).to.throw('Can\'t merge array into object');
 	});
 });
